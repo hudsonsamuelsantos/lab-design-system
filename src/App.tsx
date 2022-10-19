dd443f2109c1529f7d3bae5326c6f52ff218cf41
Hudson Santos <hudsnss@gmail.com>
2022-10-19 05:51:28 -0300

    add logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Logo } from './Logo'
 import './styles/global.css'
 
 export function App() {
   return (
-    <h1 className='text-2xl' >Hello World</h1>
+    <div className='w-screen h-screen bg-gray-900 flex items-center justify-center text-gray-100' >
+      <Logo />
+    </div>
   )
 }
