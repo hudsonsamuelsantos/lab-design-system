ae54de8782576cb8eb100af5617ffecd28bbbbbc
Hudson Santos <hudsnss@gmail.com>
2022-10-25 23:56:31 -0300

    storybook tests

--- src/pages/SignIn.stories.tsx
+++ src/pages/SignIn.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
 import { SignIn } from './SignIn'
 
 export default {
@@ -9,4 +11,17 @@ export default {
 
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj = {
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement)
+
+        userEvent.type(canvas.getByPlaceholderText('Digite seu e-mail'), 'teste@teste.com')
+        userEvent.type(canvas.getByPlaceholderText('********'), '12345678')
+
+        userEvent.click(canvas.getByRole('button'))
+
+        await waitFor(() => {
+            return expect(canvas.getByText('Login Realizado!')).toBeInTheDocument()
+        })
+    }
+}
